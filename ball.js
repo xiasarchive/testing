2e0be4219bb63c7ab45d5fcf4fb19abefeb51096
xiasarchive <xiadgarcia@gmail.com>
2025-02-10 12:39:13 -0600

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,31 @@
+class Ball{
+  constructor(x, y, z){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.dy = 1;
+    this.opac = 255;
+  }
+  drawText(){
+    fill(0,0,0, this.opac)
+    text(nameItems[this.z],this.x, this.y, 50)
+  }
+  
+  moveText(){
+    
+    this.y = this.y + this.dy;
+}
+  fadeText(){
+    if (frameCount % 5 == 0) {
+      this.opac = this.opac - 1;
+    }
+  }
+
+  checkBoundary(){
+    
+    if (this.y > height+15){
+      this.y = -15;
+    }
+  }
+  
+}

a9e8dddf2cae09472febc00908dc1555575c49e6
xiasarchive <xiadgarcia@gmail.com>
2025-02-10 19:21:22 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let balls = [];
+const nameItems = [];
+let capture1;
+let buttonClick = -1;
+
+let nameInput;
+let button;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  capture1 = createCapture(VIDEO);
+  capture1.hide();
+  
+  nameInput = select("#textInput");
+  nameInput.position(width/2-203, windowHeight-250);
+  nameInput.size(400, 150);
+  
+  button = select("#textButton");
+  button.position(width/2 - 203, nameInput.y+165);
+  button.size(406, 30)
+  button.mousePressed(greet);
+  
+  
+
+}
+
+function draw() {
+  
+  image(capture1,0, 0, width, height)
+  for (let i=0; i<balls.length; i++){
+    balls[i].drawText();
+    balls[i].moveText();
+    balls[i].checkBoundary();
+    balls[i].fadeText();
+  }
+  
+  for (let j = 0; j<nameItems.length; j++) {
+   let name = nameInput.value(); 
+   text(name, 0, 0);
+    }
+
+  
+}
+
+
+
+
+function greet(){
+  buttonClick += 1;
+  balls.push(new Ball(random(width), random(height), buttonClick));
+  
+  
+  nameItems.push(nameInput.value());  
+  
+}
